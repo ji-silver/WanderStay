e81203d81693a38092cab4111d75fde344f37e98
jisilver <jisilver.kim@gmail.com>
2023-05-26 03:37:31 +0900

    상위폴더 생성

--- utils/error.js
+++
@@ -1,7 +0,0 @@
-// createError()는 에러객체 생성하기 위한 함수 (상태코드, 메세지를 입력받아 객체로 반환)
-export const createError = (status, message) => {
-  const err = new Error();
-  err.status = status;
-  err.message = message;
-  return err;
-};
